248099b5083cb80da1dded3e88deb6e32f00d467
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2022-10-23 17:58:49 +0900

    text

--- app/src/main/java/com/example/android_beginner/MainActivity.kt
+++ app/src/main/java/com/example/android_beginner/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.android_beginner
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,11 +13,15 @@ class MainActivity : AppCompatActivity() {
             R.layout.activity_main
         )
         val rollButton: Button = findViewById(R.id.roll_button)
+
+
         rollButton.setOnClickListener{ rollDice() }
     }
 
     private fun rollDice() {
-        Toast.makeText(this, "onClickedRoll",
-            Toast.LENGTH_SHORT).show()
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice Rolled!"
+//        Toast.makeText(this, "onClickedRoll",
+//            Toast.LENGTH_SHORT).show()
     }
 }
